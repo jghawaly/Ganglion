Fixed bug: adaptation parameter was having opposite than intended effect due to flipped sign in v_m update equation, it was also not being updated at neuron firing

ganglion/vectorized/NeuralGroup.py
@@ -159,7 +159,7 @@ class AdExNeuralGroup(NeuralGroup):
         refrac = self.not_in_refractory()
 
         # calculate change in membrane potential for neurons not in refractory period
-        dvm = self.tki.dt() * ((self.sf * np.exp((self.v_m - self.v_thr) / self.sf) - (self.v_m - self.v_r)) / self.tao_m - (i_syn - self.w) / self.c_m) * refrac
+        dvm = self.tki.dt() * ((self.sf * np.exp((self.v_m - self.v_thr) / self.sf) - (self.v_m - self.v_r)) / self.tao_m - (i_syn + self.w) / self.c_m) * refrac
 
         # update membrane potential
         self.v_m += dvm
@@ -169,7 +169,6 @@ class AdExNeuralGroup(NeuralGroup):
         self.w += dw
 
         # find indices of neurons that have fired
-        
         self.spiked = np.where(self.v_m >= self.v_thr)
 
         if np.nan in self.v_m:
@@ -191,6 +190,9 @@ class AdExNeuralGroup(NeuralGroup):
 
         # change the actual membrane voltage to the resting potential for each neuron that fired
         self.v_m[self.spiked] = self.v_r[self.spiked]
+
+        # reset the w parameter
+        self.w[self.spiked] += self.b[self.spiked]
 
         # if we are tracking any variables, then append them to their respective lists, Note: This can use a lot of memory and cause slowdowns, so only do this when absolutely necessary
         if "v_m" in self.tracked_vars:

ganglion/vectorized/SynapticGroup.py
@@ -28,20 +28,20 @@ def isyn_jit(history, w, v_m_post, v_rev_pre, gbar_pre, decayed_time):
     return np.sum(history * w * (v_m_post - v_rev_pre) * gbar_pre * decayed_time)
 
 @jit(nopython=True, parallel=True, nogil=True, fastmath=True)
-def isyn_jit_parallel(history, w, v_m_post, v_rev_pre, gbar_pre, decayed_time):
+def isyn_jit_parallel(history, w, v_m_post, v_rev_pre, gbar_pre, decayed_time, weight_multiplier):
     """
     Calculate synaptic current, JIT compiled for faster processing
     """
-    return history * w * (v_m_post - v_rev_pre) * gbar_pre * decayed_time
+    return history * w * (v_m_post - v_rev_pre) * gbar_pre * decayed_time * weight_multiplier
 
 @jit(nopython=True, parallel=True, nogil=True)
-def isyn_jit_full_parallel(history, w, v_m_post, v_rev_pre, gbar_pre, decayed_time):
+def isyn_jit_full_parallel(history, w, v_m_post, v_rev_pre, gbar_pre, decayed_time, weight_multiplier):
     """
     Run the isyn calculation for all timesteps in the history array and return the resulting array
     """
     res = np.zeros_like(history)
     for i in prange(history.shape[0]):
-        res[i] = isyn_jit_parallel(history[i], w, v_m_post, v_rev_pre, gbar_pre, decayed_time[i])
+        res[i] = isyn_jit_parallel(history[i], w, v_m_post, v_rev_pre, gbar_pre, decayed_time[i], weight_multiplier)
     return res
 
 class SynapticGroup:
@@ -137,12 +137,16 @@ class SynapticGroup:
         a = np.zeros((self.m, self.n))
         a[f, :] = fired_neurons[f, None]
 
+        # we don't want to update synapses with zero weight
+        mask = self.w.copy()
+        mask[np.where(self.w > 0.0)] = 1.0
+
         self.stdp_r1[f, :] += 1
         self.stdp_r2[f, :] += 1
         
         self.roll_history_and_assign(a)
         if self.trainable:
-            self._stdp(a, 'pre') 
+            self._stdp(a * mask, 'pre') 
 
     def post_fire_notify(self, fired_neurons):
         """
@@ -154,17 +158,16 @@ class SynapticGroup:
         f = np.where(fired_neurons>0.5)
         a = np.zeros((self.m, self.n))
         a[:, f] = fired_neurons[f]
-        # if np.sum(fired_neurons) >0:
-        #     print(fired_neurons)
-        #     print(a)
-        #     print(self.stdp_o1)
-        #     exit()
+
+        # we don't want to update synapses with zero weight
+        mask = self.w.copy()
+        mask[np.where(self.w > 0.0)] = 1.0
         
         self.stdp_o1[:, f] += 1
         self.stdp_o2[:, f] += 1
 
         if self.trainable:
-            self._stdp(a, 'post')  
+            self._stdp(a * mask, 'post')  
 
     def _stdp(self, fired_neurons, fire_time):
         """
@@ -230,6 +233,6 @@ class SynapticGroup:
         # return isyn_jit(self.history, self.w, v_m_post, v_rev_pre, gbar_pre, self.time_decay_matrix) # this one is even faster and works
 
         # this is the fastest, and works
-        return np.sum(isyn_jit_full_parallel(self.history, self.w, v_m_post, v_rev_pre, gbar_pre, self.time_decay_matrix))
+        return np.sum(isyn_jit_full_parallel(self.history, self.w, v_m_post, v_rev_pre, gbar_pre, self.time_decay_matrix, self.weight_multiplier))
 
         

ganglion/vectorized/ex1.py
@@ -11,19 +11,19 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    # mode = "single_neuron"
-    mode = "single_network"
+    mode = "single_neuron"
+    # mode = "single_network"
     # single neuron simulation
     if mode == "single_neuron":
         tki = TimeKeeperIterator(timeunit=0.1*msec)
-        duration = 1000.0 * msec
+        duration = 10000.0 * msec
         n = AdExNeuralGroup(np.ones((1,1), dtype=np.int), "George", tki, AdExParams())
 
         vms = []
 
         for step in tki:
             # constant current input
-            vms.append(n.run(-1 * namp)[0][0])
+            vms.append(n.run(-1.0 * namp)[0][0])
             
             sys.stdout.write("Current simulation time: %g milliseconds\r" % (step * tki.dt() / msec))
 

ganglion/vectorized/ex3.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 
 
 tki = TimeKeeperIterator(timeunit=0.5*msec)
-duration = 1000.0 * msec
+duration = 300000.0 * msec
 g1 = SensoryNeuralGroup(np.ones(784, dtype=np.int), "inputs", tki, AdExParams(), field_shape=(28, 28))
 g2 = AdExNeuralGroup(np.ones(36, dtype=np.int), "exc", tki, AdExParams(), field_shape=(6,6))
 g3 = AdExNeuralGroup(np.zeros(36, dtype=np.int), "inh_lateral", tki, AdExParams(), field_shape=(6,6))
@@ -21,7 +21,7 @@ g4 = AdExNeuralGroup(np.zeros(36, dtype=np.int), "inh_lateral2", tki, AdExParams
 
 nn = NeuralNetwork([g1, g2, g3, g4], "mnist", tki)
 lp = STDPParams()
-lp.lr = 0.001
+lp.lr = 0.0005
 sp = SynapseParams()
 sp.spike_window = 15.0 * msec
 nn.fully_connect("inputs", "exc", trainable=True, stdp_params=lp, minw=0.01, maxw=0.4, syn_params=sp)

ganglion/vectorized/ex4.py
@@ -92,7 +92,7 @@ if __name__ == "__main__":
             g1.run(np.zeros(g1.field_shape, dtype=np.int))
         else:
             # inject spikes into sensory layer
-            g1.run(poisson_train(d, tki.dt(), 20))
+            g1.run(poisson_train(d, tki.dt(), 5))
         # run all layers
         nn.run_order(["inputs", "exc", "inh_lateral"])
         